c07137f09632d613c82935a780c012f27bab0495
Fahad Patel <42268318+fahad1403@users.noreply.github.com>
2020-04-29 08:06:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'http://s3.amazonaws.com/corona-predict/my_model.h5'
+MODEL_PATH = 'my_model.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
